5b08f1df9c9fdaa2e5dd86bba5e3254ea38876dc
omerSuliman <omerahmed41@gmail.com>
2023-06-07 10:06:57 +0200

    - updated test_output_writer and created test_output_writer_format.

--- infrastructure/test/test_output_writer.py
+++ infrastructure/test/test_output_writer.py
@@ -7,3 +7,15 @@ def test_output_writer():
     output_writer = OutputWriter("output")
     output_writer.write_html_table(["dummy_data"], ["dummy_header"], "file_name.html")
     assert os.path.exists(output_file)
+    os.remove(output_file)
+
+
+def test_output_writer_format():
+    output_file = "./output/file_name.html"
+    output_writer = OutputWriter("output")
+    output_writer.write_html_table(["dummy_data"], ["dummy_header"], "file_name.html")
+    with open(output_file, 'r') as file:
+        content = file.read()
+        assert content.startswith("<table")
+        assert content.endswith("</table>")
+    os.remove(output_file)
